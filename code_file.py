b24c78f7ce50650226b4bd4236e2958be39d9d9b
Angha01 <jadhav.angha1105@gmail.com>
2025-05-06 12:00:18 +0530

    Add files via upload

---
+++ code_file.py
@@ -0,0 +1,8 @@
+# writes a code that takes positive integer n as input and print that sum of all numbers from 1 to n
+input =5
+
+count=0
+n=6
+for  i in range(n):
+    count=count+i
+    print(count,end=" ")
